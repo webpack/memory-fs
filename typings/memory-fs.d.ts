8b31f7b77071cfed70b1ae9e0c5cfb5d3640a88f
e-cloud <saintscott119@gmail.com>
2016-12-08 16:27:07 +0800

    sync: update with upstream

--- typings/memory-fs.d.ts
+++ typings/memory-fs.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for memory-fs 0.3.0
+// Type definitions for memory-fs 0.4.1
 // Project: https://github.com/webpack/memory-fs
 // Definitions by: e-cloud <https://github.com/e-cloud>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
