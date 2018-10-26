27957fc8436f7628b0ebdc82cd02568041d10324
VijayKrish93 <vjkrishna689@gmail.com>
2018-10-26 11:29:46 +0530

    liniting done

--- test.js
+++
@@ -1,7 +0,0 @@
-const MemoryFileSystem = require('./lib/MemoryFileSystem');
-const fs = new MemoryFileSystem();
-fs.mkdirpSync('/a/b/c');
-debugger;
-fs.rmdirSync('/a');
-console.log(fs.readdirSync('/b'))
-process.exit(1)
