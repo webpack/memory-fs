8bafc544cf2ff7b2c9541b029a4747a6eed51c20
Grady Kuhnline <github@heygrady.net>
2019-02-09 23:44:58 -0800

    formatting

--- test/transformData.js
+++ test/transformData.js
@@ -6,53 +6,53 @@ var transformData = require("../lib/transformData");
 var MemoryFileSystem = require("../lib/MemoryFileSystem");
 
 describe("transformData", function() {
-  var data
-  beforeEach(function() {
-    data = {
-    	"": true,
-    	a: {
-    		"": true,
-    		index: new Buffer("1"), // /a/index
-    		dir: {
-    			"": true,
-    			index: new Buffer("2") // /a/dir/index
-    		},
-        b: {"": true}
-    	},
-    	"C:": {
-    		"": true,
-    		a: {
-    			"": true,
-    			index: new Buffer("3"), // C:\files\index
-    			dir: {
-    				"": true,
-    				index: new Buffer("4") // C:\files\a\index
-    			},
-          b: {"": true}
-    		}
-    	}
-    }
-  })
+	var data
+	beforeEach(function() {
+		data = {
+			"": true,
+			a: {
+				"": true,
+				index: new Buffer("1"), // /a/index
+				dir: {
+					"": true,
+					index: new Buffer("2") // /a/dir/index
+				},
+				b: {"": true}
+			},
+			"C:": {
+				"": true,
+				a: {
+					"": true,
+					index: new Buffer("3"), // C:\files\index
+					dir: {
+						"": true,
+						index: new Buffer("4") // C:\files\a\index
+					},
+					b: {"": true}
+				}
+			}
+		}
+	})
 	it("should transform data", function() {
-    const memfsData = transformData(data)
-    memfsData.should.be.eql({
-      '/a/b': null,
-      '/a/dir/index': '2',
-      '/a/index': '1',
-      'C:\\a\\b': null,
-      'C:\\a\\dir\\index': '4',
-      'C:\\a\\index': '3'
-    });
-  });
+		const memfsData = transformData(data)
+		memfsData.should.be.eql({
+			'/a/b': null,
+			'/a/dir/index': '2',
+			'/a/index': '1',
+			'C:\\a\\b': null,
+			'C:\\a\\dir\\index': '4',
+			'C:\\a\\index': '3'
+		});
+	});
 	it("should create volume with data", function() {
-    const fs = new MemoryFileSystem(data)
-    fs.toJSON().should.be.eql({
-      '/a/b': null,
-      '/a/dir/index': '2',
-      '/a/index': '1',
-      'C:\\a\\b': null,
-      'C:\\a\\dir\\index': '4',
-      'C:\\a\\index': '3'
-    });
-  });
+		const fs = new MemoryFileSystem(data)
+		fs.toJSON().should.be.eql({
+			'/a/b': null,
+			'/a/dir/index': '2',
+			'/a/index': '1',
+			'C:\\a\\b': null,
+			'C:\\a\\dir\\index': '4',
+			'C:\\a\\index': '3'
+		});
+	});
 })
--- test/transformJSON.js
+++ test/transformJSON.js
@@ -5,51 +5,51 @@ var transformJSON = require("../lib/transformJSON");
 var MemoryFileSystem = require("../lib/MemoryFileSystem");
 
 describe("transformJSON", function() {
-  var json
-  var data
-  beforeEach(function() {
-    json = {
-      '/a/b': null,
-      '/a/dir/index': '2',
-      '/a/index': '1',
-      'C:\\a\\b': null,
-      'C:\\a\\dir\\index': '4',
-      'C:\\a\\index': '3'
-    };
-    data = {
-      "": true,
-      a: {
-        "": true,
-        b: {"": true},
-        dir: {
-          "": true,
-          index: new Buffer("2") // /a/dir/index
-        },
-        index: new Buffer("1") // /a/index
-      },
-      "C:": {
-        "": true,
-        a: {
-          "": true,
-          b: {"": true},
-          dir: {
-            "": true,
-            index: new Buffer("4") // C:\files\a\index
-          },
-          index: new Buffer("3") // C:\files\index
-        }
-      }
-    };
-  })
+	var json
+	var data
+	beforeEach(function() {
+		json = {
+			'/a/b': null,
+			'/a/dir/index': '2',
+			'/a/index': '1',
+			'C:\\a\\b': null,
+			'C:\\a\\dir\\index': '4',
+			'C:\\a\\index': '3'
+		};
+		data = {
+			"": true,
+			a: {
+				"": true,
+				b: {"": true},
+				dir: {
+					"": true,
+					index: new Buffer("2") // /a/dir/index
+				},
+				index: new Buffer("1") // /a/index
+			},
+			"C:": {
+				"": true,
+				a: {
+					"": true,
+					b: {"": true},
+					dir: {
+						"": true,
+						index: new Buffer("4") // C:\files\a\index
+					},
+					index: new Buffer("3") // C:\files\index
+				}
+			}
+		};
+	})
 	it("should transform json", function() {
-    transformJSON(json).should.be.eql(data);
-  });
+		transformJSON(json).should.be.eql(data);
+	});
 	it("should create volume with json", function() {
-    const fs = new MemoryFileSystem(null, json)
-    fs.toJSON().should.be.eql(json);
-  });
-  it("should export data", function() {
-    const fs = new MemoryFileSystem(null, json)
-    fs.data.should.be.eql(data);
-  });
+		const fs = new MemoryFileSystem(null, json)
+		fs.toJSON().should.be.eql(json);
+	});
+	it("should export data", function() {
+		const fs = new MemoryFileSystem(null, json)
+		fs.data.should.be.eql(data);
+	});
 })
